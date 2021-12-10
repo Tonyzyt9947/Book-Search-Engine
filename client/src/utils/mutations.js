c9a4d8a1ee4eb2549d2b2103c6d30133b7226468
TZ <tz116917@gmail.com>
2021-12-09 16:48:11 -0800

    bugfix -wrong import in mutations

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,4 +1,4 @@
-import { gql } from 'graphql-tag';
+import { gql } from '@apollo/client';
 
 export const ADD_USER = gql`
     mutation addUser($username: String!, $email: String!, $password: String!){
@@ -7,23 +7,12 @@ export const ADD_USER = gql`
             user{
                 _id
                 username
-                bookCount
-                email
-                savedBooks{
-                    title
-                    bookId
-                    link
-                    description
-                    authors
-                    image
-                    
-                }
             }    
         }
     }`;
 
 export const LOGIN_USER = gql`
-    mutation loginUser($email: String!, $password: String!) {
+    mutation login($email: String!, $password: String!) {
         login(email: $email, password: $password) {
             token
             user {
@@ -31,16 +20,13 @@ export const LOGIN_USER = gql`
                 username
             }
         }
-    }
-`;
+    }`;
 
 export const REMOVE_BOOK = gql`
     mutation removeBook($bookId: String!) {
         removeBook(bookId: $bookId) {
             _id
             username
-            bookCount
-            email
             savedBooks{
                 title
                 bookId
